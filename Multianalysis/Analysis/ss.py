0e6ead1d0d983b99cf977bad624fc891eb0f9f9c
Helena García-Cebollada <139923595+elhectro2@users.noreply.github.com>
2026-02-05 16:51:14 +0100

    Update ss.py

--- Multianalysis/Analysis/ss.py
+++ Multianalysis/Analysis/ss.py
@@ -21,18 +21,35 @@ def calculate_ss(input_dir, output_dir, replica, protein_type):
 
 
 def read_ss(output_dir, replica):
+        if protein_type and protein_type.lower() == "alpha":
+        sscodes = ["Î±-Helices"]
+    elif protein_type and protein_type.lower() == "beta":
+        sscodes = ["Î²-Strands", "Î²-Bridges"]
+    else:
+        sscodes = ["Î±-Helices", "Î²-Strands", "Î²-Bridges"]
     file_dir = "%ssscount_%s.xvg" % (output_dir, replica)
     data = []
     time_series = []
     values = []
-    file = open(file_dir)
+    indices = []
+    file = open(file_dir, errors="ignore")
     for line in file:  # Reads all the file and does things
+        print(line)
         if line[0] not in "#@":  # Non-comment lines
             line = line.split()  # Splits the line in a list of "words" (elements separated by spaces in a string)
             data.append(line)  # Data for each time is stored in a list of lists
+        elif line[0:3] == "@ s":
+            for code in sscodes:
+                if code in line[3:]:
+                    indices.append(int(line[3:].split()[0]) + 1)
+                    break
+    print(indices)
     for item in data:
         time_series.append(float(item[0]))
-        values.append(float(item[1]))
+        if indices:
+            values.append(sum([float(item[index]) for index in indices]))
+        else:
+            values.append(0)
     return [time_series, values]
 
 
@@ -41,14 +58,15 @@ def read_ss_coil(output_dir, replica):
     data = []
     time_series = []
     values = []
-    file = open(file_dir)
+    file = open(file_dir, errors="ignore")
     index = False
     for line in file:  # Reads all the file and does things
         if line[0] not in "#@":  # Non-comment lines
             line = line.split()  # Splits the line in a list of "words" (elements separated by spaces in a string)
             data.append(line)  # Data for each time is stored in a list of lists
-        elif line[0:3] == "@ s" and "Coil" in line[3:]:
+        elif line[0:3] == "@ s" and "Loops" in line[3:]:
             index = int(line[3:].split()[0]) + 1
+    print(index)
     for item in data:
         time_series.append(float(item[0]))
         if index:
